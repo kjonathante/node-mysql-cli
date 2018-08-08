3f4c13a67d1a2f684e8d0d24eb765fa7206c7dfb
Kit Jonathan Te <kjonathante@gmail.com>
2018-08-08 16:28:57 -0700

    add inquirer and preliminary questions

--- app.js
+++ app.js
@@ -15,5 +15,24 @@ conn.connect( function(error){
   }
   //console.log( arguments )
   // start app
-  conn.end()
+  main()
 })
+
+function main() {
+  inquirer.prompt([ {
+    type: 'list',
+    name: 'id',
+    message: 'Enter you product selection: ',
+    choices: ['selection 1','selection 2'],
+  },{
+    type: 'input',
+    name: 'qty',
+    message: 'How many units would you like to buy ? ',
+    validate: function() { return true },
+  }]).then(
+    function(answers) {
+      console.log(answers)
+      conn.end()
+    }
+  )
+}
