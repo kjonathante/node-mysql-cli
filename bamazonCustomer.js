9f7d6085b856e0488c4f2a28778140750cc33425
Kit Jonathan Te <kjonathante@gmail.com>
2018-08-09 15:17:13 -0700

    update bamazonCustomer to work with a new column added to the products table. product_sales column is updated each time a customer bought some merchandize.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -58,7 +58,7 @@ function getProducts() {
         for( var row of results ) {
           arr.push({
             name: row.product_name.padEnd(20) + ' ' + row.price.toFixed(2).padStart(10),
-            value: {id: row.id, price: row.price, qty: row.stock_quantity},
+            value: {id: row.id, price: row.price, qty: row.stock_quantity, productSales: row.product_sales},
             short: row.product_name + ' @ $' + row.price.toFixed(2),
           })
         }
@@ -70,21 +70,22 @@ function getProducts() {
 
 function checkQty(qty, answer) {
   return new Promise( function(resolve, reject) {
+    var quantity = parseFloat(qty);
     conn.query({
       sql: 'SELECT stock_quantity FROM products WHERE id=?',
       values: [answer.item.id]
     }, function(error, results, fields){
       if (error) throw reject(error)
       
-      resolve(results[0].stock_quantity>=qty? true : 'Insufficient quantity!')
+      resolve(results[0].stock_quantity>=quantity? true : 'Insufficient quantity!')
     })
   })
 }
 
 function setQty(answers) {
   conn.query({
-    sql: 'UPDATE products SET stock_quantity=? WHERE id=?',
-    values: [answers.item.qty-answers.qty, answers.item.id]
+    sql: 'UPDATE products SET stock_quantity=?, product_sales=? WHERE id=?',
+    values: [answers.item.qty-answers.qty, answers.item.productSales + (answers.qty*answers.item.price), answers.item.id]
   }, function(error, results, fields){
     if (error) throw error
     console.log('Completed.')
